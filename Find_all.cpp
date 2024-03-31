93c9711916dfb392e89c4367dc42a544aa7ee95f
Kaitrye <93823684+Kaitrye@users.noreply.github.com>
2024-03-31 14:23:55 +0300

    Add search size and angles

--- Find_all.cpp
+++ Find_all.cpp
@@ -8,7 +8,8 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 bool loadCamParams (const std::string& filename, cv::Mat& cameraMat, cv::Mat& distCoefs)
 {
@@ -19,7 +20,7 @@ bool loadCamParams (const std::string& filename, cv::Mat& cameraMat, cv::Mat& di
 		int rows;
 		int columns;
 
-		// Ã‡ÚËˆ‡ Í‡ÏÂ˚.
+		// –ú–∞—Ç—Ä–∏—Ü–∞ –∫–∞–º–µ—Ä—ã.
 		inFile >> rows;
 		inFile >> columns;
 
@@ -36,7 +37,7 @@ bool loadCamParams (const std::string& filename, cv::Mat& cameraMat, cv::Mat& di
 			}
 		}
 
-		// ¬ÂÍÚÓ ÍÓ˝ÙÙËˆËÂÌÚÓ‚.
+		// –í–µ–∫—Ç–æ—Ä –∫–æ—ç—Ñ—Ñ–∏—Ü–∏–µ–Ω—Ç–æ–≤.
 		inFile >> rows;
 		inFile >> columns;
 
@@ -63,26 +64,36 @@ bool loadCamParams (const std::string& filename, cv::Mat& cameraMat, cv::Mat& di
 
 int main ()
 {
-	const char* filename = "sizes2.txt";
-	std::ofstream outFile (filename);
+	const char* result = "characters.txt";
+	std::ofstream outFile (result);
 	if (!outFile)
 	{
-		std::cout << "Can not open file " << filename << std::endl;
+		std::cout << "Can not open file " << result << std::endl;
 		return 1;
+	}
+
+	const char* sizes = "currentASize.txt";
+	std::ifstream inFile (sizes);
+	if (!inFile)
+	{
+		std::cout << "Can not open file " << sizes << std::endl;
+		return 2;
 	}
 
 	cv::Mat distCoefs = cv::Mat::zeros (8, 1, CV_64F);
 	cv::Mat cameraMat = cv::Mat::eye (3, 3, CV_64F);
-	const double aruco_size = 0.033f;
+	const double aruco_size[4] = {0.033f, 0.076f, 0.105f, 0.156f};
 
 	loadCamParams("params.txt", cameraMat, distCoefs);
 
 	cv::Mat frame;
-	std::string indir = "Exp_1";
+	std::string indir = "dataset";
+	std::string outdir = "axis";
+	size_t ar_index;
 
-	for (int i = 1; i <= 10; ++i)
+	for (int i = 1; i <= 2540; ++i)
 	{
-		std::string filename = "IMG (" + std::to_string (i) + ").jpg"; // ËÏˇ ËÁÓ·‡ÊÂÌËˇ.
+		std::string filename = "IMG_" + std::to_string (i) + ".jpg"; // –∏–º—è –∏–∑–æ–±—Ä–∞–∂–µ–Ω–∏—è.
 		std::string path = indir + "/" + filename;
 
 		frame = cv::imread (path, cv::IMREAD_COLOR);
@@ -97,7 +108,9 @@ int main ()
 
 		cv::resize (frame, frame, cv::Size (new_width, new_height), cv::INTER_LINEAR);
 
-		// Ó·Ì‡ÛÊÂÌËÂ ‡ÛÍÓ-Ï‡ÍÂÓ‚.
+		inFile >> ar_index;
+
+		// –æ–±–Ω–∞—Ä—É–∂–µ–Ω–∏–µ –∞—Ä—É–∫–æ-–º–∞—Ä–∫–µ—Ä–æ–≤.
 		std::vector<int> markerIds;
 		std::vector<std::vector<cv::Point2f>> markerCorners;
 		std::vector<cv::Ptr<cv::aruco::Dictionary>> array_dict (4);
@@ -117,22 +130,47 @@ int main ()
 		if (markerIds.size () > 0)
 		{
 			std::vector<cv::Vec3d> tVectors, rVectors;
-			cv::aruco::estimatePoseSingleMarkers (markerCorners, aruco_size, cameraMat, distCoefs, rVectors, tVectors);
+			cv::aruco::estimatePoseSingleMarkers (markerCorners, aruco_size[ar_index], cameraMat, distCoefs, rVectors, tVectors);
 			cv::aruco::drawDetectedMarkers (frame, markerCorners, markerIds);
 			cv::aruco::drawAxis (frame, cameraMat, distCoefs, rVectors[0], tVectors[0], 0.05f);
-			double x = tVectors[0] (0);
-			double y = tVectors[0] (1);
+			
+			cv::Mat rMatrix;
+			cv::Rodrigues(rVectors[0], rMatrix);
+
+			/*std::cout << filename << "\nMatrix:\n";
+
+			for (int row = 0; row < 3; ++row)
+			{
+				for (int col = 0; col < 3; ++col)
+				{
+					std::cout << rMatrix.at <double> (row, col) << " ";
+				}
+				std::cout << "\n";
+			}*/
+
+			double beta = std::asin(rMatrix.at <double> (2, 0));
+			double alpha = std::asin(rMatrix.at <double> (1, 0) / std::cos(beta));
+			double gamma = std::asin(rMatrix.at <double> (2, 1) / std::cos(beta));
 			double z = tVectors[0] (2);
 
-			outFile << filename << ": " << std::sqrt (x * x + y * y + z * z) << std::endl;
-			outFile << "x: " << x << std::endl;
-			outFile << "y: " << y << std::endl;
-			outFile << "z: " << z << std::endl;
-			outFile << std::endl;
+			/**std::cout << "Angles:\n";
+			std::cout << gamma * 180 / M_PI << "\n" << beta * 180 / M_PI << "\n" << alpha * 180 / M_PI << std::endl;
+
+			std::cout << "Size: " << z << std::endl;*/
+
+			outFile << i << "," << z << "," << gamma * 180 / M_PI << "," << beta * 180 / M_PI << "," << alpha * 180 / M_PI << "," << 1 << "," << markerIds[0] << "," << aruco_size[ar_index] << std::endl;
 		}
+		else
+		{
+			outFile << i << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << aruco_size[ar_index] << std::endl;
+		}
+
+		std::string output = outdir + "/" + filename;
+
+		imwrite (output, frame);
 		
-		imshow ("Ã‡ÍÂ˚ Ë ÓÒË", frame);
-		cv::waitKey (0);
+		/*imshow ("–ú–∞—Ä–∫–µ—Ä—ã –∏ –æ—Å–∏", frame);
+		cv::waitKey (0);*/
 	}
 
 	outFile.close();
