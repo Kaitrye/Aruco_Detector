50cd8d517eadcb62e7d8772376bc46725b486505
Kaitrye <93823684+Kaitrye@users.noreply.github.com>
2025-01-26 03:39:34 +0300

    Update calib.h

--- calib.h
+++ calib.h
@@ -19,6 +19,6 @@ extern const float lengthChess;
 
 bool loadCamParams (const char* filename, cv::Mat& cameraMat, cv::Mat& distCoefs);
 bool saveCamParams (const char* filename, const cv::Mat& cameraMat, const cv::Mat& distCoefs);
-bool Calibration (unsigned char num_camera = 0, , const std::string& path = "./calib_photos");
+bool Calibration (unsigned char num_camera = 0, const std::string& path = "./calib_photos");
 
 #endif //CAM_CALIB_
